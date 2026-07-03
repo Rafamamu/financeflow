03378e8f3b592095adc5fe02db660d54aecef1e5
Rafael <rafaroga46@gmail.com>
2026-07-02 21:48:50 -0300

    feat: add /meuPerfil endpoint to extract authenticated user's email from SecurityContextHolder.

--- src/main/java/com/rafamamu/financeflow/controller/UserController.java
+++ src/main/java/com/rafamamu/financeflow/controller/UserController.java
@@ -5,6 +5,8 @@ import com.rafamamu.financeflow.dto.UserResponseDTO;
 import com.rafamamu.financeflow.model.User;
 import com.rafamamu.financeflow.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,12 @@ public class UserController {
     @PostMapping("/userRegister")
     public UserResponseDTO createUser(@Valid @RequestBody UserRequestDTO  request) {
         return  userService.createUser(request);
+    }
+
+    @GetMapping("/meuPerfil")
+    public String getPerfil() {
+        return "Segue as credenciais do Usuário: "+
+                SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
     }
 
 
