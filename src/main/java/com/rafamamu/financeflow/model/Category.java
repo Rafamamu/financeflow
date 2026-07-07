8747e7914b6c47fd2bebf4c781b86be387b1c930
Rafael <rafaroga46@gmail.com>
2026-07-06 22:23:06 -0300

    feat: create Category entity, repository and Flyway migration.

---
+++ src/main/java/com/rafamamu/financeflow/model/Category.java
@@ -0,0 +1,45 @@
+package com.rafamamu.financeflow.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
+@Builder
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category() {
+
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+       return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+
+
+}
